a5e1562e51530015b71cb2988f36edf4a37f7e9a
UlianaPereverzeva <115875780+UlianaPereverzeva@users.noreply.github.com>
2022-11-29 16:02:38 +0300

    do UserDefaultsService ana UserDefaultsextension

--- AppRegistration/AppRegistration/Controllers/WelcomeViewController.swift
+++ AppRegistration/AppRegistration/Controllers/WelcomeViewController.swift
@@ -17,20 +17,17 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setUpUI()
+        
     }
     
 
     @IBAction func continueButtonAction() {
+        navigationController?.popToRootViewController(animated: true)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    private func setUpUI() {
+        infoLabel.text = "Nice to meet you \(userModel?.name)"
     }
-    */
 
 }
